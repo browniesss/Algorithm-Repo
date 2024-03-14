9a81d0f83922eab2800f7247cd1b1dcb644be0ea
이정우 <91866397+browniesss@users.noreply.github.com>
2024-03-14 21:45:16 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+ 
+ 
+int main() {
+	int N, B; // 10진법 수 N, B진법
+	cin >> N >> B;
+ 
+	string result = "";
+ 
+	while (N>0)
+	{
+		int r = N % B;
+ 
+		if (r<10)
+		{
+			result += (char)(r + '0');
+			// '0'은 ASCII로 48 -> 문자로 변환시켜주려면 int값에 '0'문자를 더해줘야 함
+		}
+		else
+		{
+			result += (char)(r - 10 + 'A');
+			// 'A'는 ASCII로 65 -> 10을 문자 A로 변환시켜주려면, 숫자 10을 빼고 'A'를 더해주거나, 55를 더해줘야 함
+		}
+ 
+		N /= B; // 나눠줘야 다음 연산 진행
+	}
+ 
+	reverse(result.begin(), result.end()); // 문자열 순서 바꿔주기
+ 
+	cout << result << '\n';
+ 
+	return 0;
+}
