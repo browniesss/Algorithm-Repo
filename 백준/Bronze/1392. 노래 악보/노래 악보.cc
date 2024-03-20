4c26169bdcd56e30610b6580cd46d052285366d6
이정우 <91866397+browniesss@users.noreply.github.com>
2024-03-20 20:34:29 +0900

    [Bronze II] Title: 노래 악보, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1392. 노래 악보/노래 악보.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define endl '\n'
+#define len(x) (int)(x).length()
+#define size(x) (int)(x).size()
+
+int main()
+{
+    // Set up : I/O
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Set up : Input
+    int N, Q;
+    cin >> N >> Q;
+    int A[N+1]; A[0] = 0;
+    for (int i=1; i<=N; i++)
+        cin >> A[i];
+
+    // Process
+    for (int i=1; i<=N; i++) {
+        A[i] += A[i-1];
+    }
+
+    // Control : Output
+    for (int i=0; i<Q; i++) {
+        int q; cin >> q;
+        cout << distance(A, upper_bound(A, A+(N+1), q)) << endl;
+    }
+}
