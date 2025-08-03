81d2ba7b35aedfa3e316ca8b0aa51cbf0b85078e
이정우 <91866397+browniesss@users.noreply.github.com>
2025-08-03 20:07:26 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+bool paper[100][100];
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (n--) {
+        int x, y;
+        cin >> x >> y;
+
+        for (int i = x; i < x + 10; ++i) {
+            for (int j = y; j < y + 10; ++j) {
+                paper[i][j] = true;
+            }
+        }
+    }
+
+    int area = 0;
+    for (int i = 0; i < 100; ++i) {
+        for (int j = 0; j < 100; ++j) {
+            if (paper[i][j]) area++;
+        }
+    }
+
+    cout << area << endl;
+    return 0;
+}
