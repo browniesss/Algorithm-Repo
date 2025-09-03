b64956d924773879e22108daba428070d6b0177b
이정우 <91866397+browniesss@users.noreply.github.com>
2025-09-03 20:07:08 +0900

    [level 3] Title: 네트워크, Time: 2.32 ms, Memory: 6.45 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,43 +1,44 @@
 #include <string>
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <math.h>
 
 using namespace std;
 
-bool visited[201];
+int minCount = 999999;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,1,0,-1 };
+vector<vector<int>> map;
+vector<bool> visited;
 
-void DFS(vector<vector<int>> computers, int index)
+void DFS(int index, vector<vector<int>> map)
 {
-    bool isEnd = true;
-    for (int i = 0; i < computers.size(); i++)
+    for (int i = 0; i < map[index].size(); i++)
     {
         if (visited[i])
-        {
             continue;
-        }
-        
-        if (computers[index][i] == 1)
+
+        if (map[index][i] == 1)
         {
             visited[i] = true;
-            DFS(computers, i);
+            DFS(i, map);
         }
     }
 }
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    
+    visited.resize(computers.size());
+
     for (int i = 0; i < computers.size(); i++)
     {
         if (visited[i])
-        {
             continue;
-        }
-        
-        visited[i] = true;
-        DFS(computers, i);
+
         answer++;
+        visited[i] = true;
+        DFS(i, computers);
     }
-    
+
     return answer;
 }
