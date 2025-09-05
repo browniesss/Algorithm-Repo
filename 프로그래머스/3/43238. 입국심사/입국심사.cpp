c8f67874c6fcbff3543fecf6530608deafd6dae7
이정우 <91866397+browniesss@users.noreply.github.com>
2025-09-05 19:21:34 +0900

    [level 3] Title: 입국심사, Time: 38.61 ms, Memory: 6.84 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,41 +1,39 @@
 #include <string>
+#include <iostream>
 #include <vector>
+#include <math.h>
 #include <algorithm>
 
 using namespace std;
 
+long long solution(int n, vector<int> times)
+{
+    long long end;
+sort(times.begin(), times.end());
 
-long long solution(int n, vector<int> times) {
-    long long answer = 0;
-    sort(times.begin(), times.end());
+end = (long long)times.back() * n; // 나올 수 있는 최대 시간 구하기
+long long answer = end;
+long long start = 1, mid = end / 2;
 
-    // 이분 탐색 세팅
-    long long start = 1;
-    long long end = (long long)times[times.size() - 1] * n;
-    long long mid;
-
-    while (start <= end)
+while (start <= end)
+{
+    mid = start + (end - start) / 2;
+    long long cnt = 0;
+    for (int i = 0; i < times.size(); i++)
     {
-        mid = (start + end) / 2;
-        long long cnt = 0;
+        cnt += mid / times[i];
+            if (cnt >= n) break;
+    }
 
-        for (int i = 0; i < times.size(); i++)
-        {
-            cnt += mid / times[i];
-        }
-
-        if (cnt < n)
-        {
-            start = mid + 1;
-        }
-        else
-        {
-            if (mid <= end)
-            {
-                answer = mid;
-            }
-            end = mid - 1;
-        }
+    if (cnt >= n)
+    {
+        answer = mid;
+        end = mid - 1;
+    }
+    else
+    {
+        start = mid + 1;
+    }
     }
 
     return answer;
